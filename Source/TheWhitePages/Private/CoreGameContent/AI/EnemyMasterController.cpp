8a1aedfaef0bc6ed59467a18be3c5249fb57e9b3
Caleb.Addi <caleb.addi.77@gmail.com>
2024-04-03 16:27:47 -0500

    Updates to enemy AI Aggro states and more

--- Source/TheWhitePages/Private/CoreGameContent/AI/EnemyMasterController.cpp
+++ Source/TheWhitePages/Private/CoreGameContent/AI/EnemyMasterController.cpp
@@ -1,2 +1,29 @@
 #include "CoreGameContent/AI/EnemyMasterController.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
+
+void AEnemyMasterController::InstantiateBehaviorTree()
+{
+    if (BehaviorTreeAsset)
+    {
+        RunBehaviorTree(BehaviorTreeAsset);
+        BlackboardComp = GetBlackboardComponent();
+    }
+}
+
+void AEnemyMasterController::SetStateAsPassive()
+{
+    if (BlackboardComp)
+    {
+        BlackboardComp->SetValueAsEnum(StateKeyName, static_cast<uint8>(EAIEnemyState::Passive));
+    }
+}
+
+void AEnemyMasterController::SetStateAsAttacking(AActor* AttackTarget)
+{
+    if (AttackTarget != nullptr && BlackboardComp)
+    {
+        BlackboardComp->SetValueAsObject(TargetKeyName, AttackTarget);
+        BlackboardComp->SetValueAsEnum(StateKeyName, static_cast<uint8>(EAIEnemyState::Aggro));
+    }
+}
--- Source/TheWhitePages/Public/CoreGameContent/AI/EnemyMasterController.h
+++ Source/TheWhitePages/Public/CoreGameContent/AI/EnemyMasterController.h
@@ -2,8 +2,18 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "BehaviorTree/BehaviorTree.h"
 #include "EnemyMasterController.generated.h"
 
+
+UENUM(BlueprintType)
+enum class EAIEnemyState : uint8
+{
+	Passive UMETA(DisplayName="Passive"),
+	Aggro UMETA(DisplayName="Aggro"),
+	Frozen UMETA(DisplayName="Frozen"),
+	Searching UMETA(DisplayName="Searching")
+};
 
 UCLASS()
 class THEWHITEPAGES_API AEnemyMasterController : public AAIController
@@ -11,12 +21,30 @@ class THEWHITEPAGES_API AEnemyMasterController : public AAIController
 	GENERATED_BODY()
 
 public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI State")
+	EAIEnemyState EnemyState;
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Tree)
+	UBehaviorTree* BehaviorTreeAsset;
 
 protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Keys)
+	FName TargetKeyName;
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Keys)
+	FName StateKeyName;
+
+	UFUNCTION(BlueprintCallable, Category = Tree)
+	void InstantiateBehaviorTree();
+
+	UFUNCTION(BlueprintCallable, Category = "AI State")
+	void SetStateAsPassive();
+
+	UFUNCTION(BlueprintCallable, Category = "AI State")
+	void SetStateAsAttacking(AActor* AttackTarget);
 
 private:
-
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category=Blackboard, meta=(AllowPrivateAccess="true"))
+	UBlackboardComponent* BlackboardComp;
 
 };
